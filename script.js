808266010c79018d8adeefb6c811741e6ed327aa
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2022-12-15 22:26:51 -0500

    commitaddedlistitems

--- script.js
+++ script.js
@@ -1,48 +1,76 @@
 // BUTTON START
-window.onclick = function(){
-    (function(){
-      var timer = 15;
-      setInterval(function() {
-        timer--;
-        if (timer >= 0) {
+// First: Target the elements by setting variables.
+// Variables at the top of the page
+// As the user clicks and answers a question, it goes onto the next page.
+const buttonStart = document.querySelector("#buttonStart");
+let questionNumber = 0;
+const questionDiv = document.querySelector("#questions")
+let answersDiv = document.querySelector("#listanswers")
+
+function startQuiz(){
+displayQuestions()
+var timeLeft = 20
+var timer = setInterval(function() {
+        timeLeft--;
+        if (timeLeft >= 0) {
           span = document.getElementById("timer");
-          span.innerHTML = "Time: " + timer;
+          span.innerHTML = "Time: " + timeLeft;
         }
-        if (timer === 0) {
+        if (timeLeft === 0) {
             alert('Sorry, out of time');
             clearInterval(timer);
         }
       }, 1000);
-    })();
+    };
+
+function displayQuestions(){
+    questionDiv.textContent = quizQuestions[questionNumber].question
+    for(let i = 0; i<=2; i++){
+    var newLi = document.createElement("li")
+        newLi.textContent = quizQuestions[questionNumber].answers[i]
+        answersDiv.appendChild(newLi)
     }
+}
+
+
+
+buttonStart.addEventListener("click", startQuiz)
+
+
+
+
+
+
+
 
 // QUIZ MATERIAL
 
-/*const quizQuestions = [
+const quizQuestions = [
     {
-        question: "What instrument did John Coltrane play?"
-        answers: {
-            a: "Trumpet"
-            b: "French Horn"
-            c: "Tenor Saxophone" 
-        },
+        question: "What instrument did John Coltrane play?",
+        answers:  ["Trumpet", "French Horn","Tenor Saxophone"],
+
+        
+         
+            
+       
         correctAnswer: "c"
     },
     {
-        question: "What instrument did Roy Haynes play?"
+        question: "What instrument did Roy Haynes play?",
         answers:{
-            a: "Tenor Saxophone"
-            b: "Drums"
+            a: "Tenor Saxophone",
+            b: "Drums",
             c: "He's not even real"
         },
         correctAnswer: "b"
-    }
+    },
     {
-        question: "What instrument did Miles Davis play?"
+        question: "What instrument did Miles Davis play?",
         answers:{
-            a: "Trumpet"
-            b: "Alto Saxophone"
-            c: "Drums"
+            a: "Trumpet",
+            b: "Alto Saxophone",
+            c: "Drums",
         },
         correctAnswer: "a"
     }
@@ -50,21 +78,3 @@ window.onclick = function(){
 console.log(quizQuestions)
 
 //THE REST OF THE STUFF
-
-function displayQuestions(quizQuestions, quizContainer)
-
-    const output = [];
-    const answers;
-
-    for(var i=0; i<quizQuestions.length; i++){
-        answers = []
-        for(letter in quizQuestions[i].answers){
-          answers.push(
-            "<label>"
-            + "<input type = 'radio'
-            name="question"
-          )  
-        }
-
-    }
-*/
