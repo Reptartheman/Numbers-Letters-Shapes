73bf0a2052eef0a28b533eeb35028a5b84fbba8e
Jeff Moro <jeffmoromusic@gmail.com>
2024-12-08 12:41:04 -0800

    fix random x bug

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ const questionDiv = document.getElementById("questions");
 const imageDisplay = document.getElementById("imageDisplay");
 const answersList = document.getElementById("answersList");
 const answerButtons = document.querySelectorAll("#answers");
-const filePath = './assets/images';
+const filePath = "./assets/images";
 let startIndex = 0;
 let timeLeft;
 
@@ -14,10 +14,13 @@ const numbers = [
   `${filePath}/4.svg`,
   `${filePath}/5.svg`,
 ];
-const shapes = [`${filePath}/Triangle.svg`, `${filePath}/Circle.svg`, `${filePath}/Square.svg`];
+const shapes = [
+  `${filePath}/Triangle.svg`,
+  `${filePath}/Circle.svg`,
+  `${filePath}/Square.svg`,
+];
 
 const theX = `${filePath}/X.svg`;
-
 
 const getRandomizedItems = (arr) => {
   return arr[Math.floor(Math.random() * arr.length)];
@@ -27,66 +30,101 @@ const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
+//const randomNumber = getRandomNumber(1, 6); //for length of X's array to be used when generating the X's images for answers
+ //array of X images to be used for display of X images
 
-const randomNumber = getRandomNumber(1, 3);
-const randomNumberImg = getRandomizedItems(numbers);
-const randomShapeImg = getRandomizedItems(shapes);
-
-const arrayOfXs = Array.from({length: randomNumber}, () => theX);
-const randomXs = getRandomizedItems(arrayOfXs);
-
-const quizData = [
-  {
-    question: "What shape is this?",
-    images: randomShapeImg,
-    answers: ['Triangle', 'Circle', 'Square'],
-    correctAnswer: function () {
-      for (let i = 0; i <= this.answers.length; i++) {
-        if (this.images.includes(this.answers[i])) {
-          return this.answers[i]
-        }
-      }
-      return null;
+const quizData = {
+  questions: [
+    "What shape is this?",
+    "What number do you see?",
+    "How many X's do you see?",
+  ],
+  images: [getRandomizedItems(shapes), getRandomizedItems(numbers)],
+  answers:
+    {
+      shapes: ["Triangle", "Circle", "Square"],
+      numbers: [1, 2, 3, 4, 5],
     },
-  },
-];
-
-console.log(randomShapeImg)
+};
 
 
 
-const questionsArray = quizData.map(question => question.question);
+const displayImagesForQuestions = (imageSrc) => {
+   imageDisplay.innerHTML = "";
+
+   const img = document.createElement("img");
+   img.src = imageSrc;
+   imageDisplay.appendChild(img);
+};
 
 
-const displayImagesForQuestions = () => {
-  const newImage = document.createElement("img");
-  newImage.src = quizData[0].images;
-  imageDisplay.appendChild(newImage);
+function displayXs() {
+  imageDisplay.innerHTML = "";
+  answersList.innerHTML = "";
+  let arrayOfXs = Array.from({ length: getRandomNumber(1, 6) }, () => theX);
+  arrayOfXs.forEach((xSrc) => {
+    const img = document.createElement("img");
+    img.src = xSrc;
+    imageDisplay.appendChild(img);
+  });
+
+  for (let i = 1; i <= 5; i++) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    button.addEventListener("click", () => {
+      prompt('hi');
+    });
+    answersList.appendChild(button);
+  }
+  
 }
 
 
-const displayAnswers = (array) => {
-
+function displayAnswers(answers) {
   answersList.innerHTML = "";
-  array.forEach((answer) => {
-    const newButton = document.createElement("button");
-    newButton.textContent = answer;
-    answersList.appendChild(newButton);
+
+  answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.textContent = answer;
+    button.addEventListener("click", () => {
+      prompt('hi');
+    });
+    answersList.appendChild(button);
   });
-};
+}
 
-function displayQuestions(array, index) {
-  questionDiv.textContent = array[index];
+function displayRandomQuestion(arr) {
 
+  questionDiv.textContent = getRandomizedItems(arr);
+
+  if (questionDiv.textContent.includes("shape")) {
+    displayAnswers(quizData.answers.shapes)
+    displayImagesForQuestions(quizData.images[0])
+  } else if (questionDiv.textContent.includes("number")) {
+    displayAnswers(quizData.answers.numbers)
+    displayImagesForQuestions(quizData.images[1])
+  } else if (questionDiv.textContent.includes("X's")) {
+    displayXs();
+  }
+
+}
+
+function displayResultForQuestion(array) {
+  array.forEach((answer) => {
+    answer.addEventListener("click", (e) => {
+      if (e.target.textContent === quizData[0].correctAnswer()) {
+        confirm("correct");
+      } else {
+        alert("wrong");
+      }
+    });
+  });
 }
 
 function startQuiz() {
-  displayQuestions(questionsArray, 0);
-  displayAnswers(quizData[0].answers);
-  displayImagesForQuestions();
+  displayRandomQuestion(quizData.questions);
 }
 
+//displayResultForQuestion(answerButtons)
 
-const currentQuestion = quizData[0];
-console.log("Correct Answer:", currentQuestion.correctAnswer());
-startQuiz();
+startButton.addEventListener("click", startQuiz);
